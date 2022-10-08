3b8ce737e533f24a31260451ba9f31b9a7524201
unknown <tamiloreolaniyi@gmail.com>
2022-10-08 06:51:40 +0100

    about-casestudy

--- src/components/casestudy/CaseStudy2.jsx
+++ src/components/casestudy/CaseStudy2.jsx
@@ -1,31 +1,94 @@
 import React from 'react'
 import './CaseStudy2.css'
+import {HiOutlineChartBar} from 'react-icons/hi'
 
 const CaseStudy2 = () => {
   return (
     <div className='casestudy2'>
-        <p>Case Study</p>
-        <h3>Case Study</h3>
-        <div className="case-study-box">
-            <div className="case-box">
-                <img src="/images/casestudy1.png" alt="" />
-                <div className="case-text">
-                    <h6>Business</h6>
-                    <h3>Business Growth</h3>
+        <div className="case-study2-top">
+            <div className="casestudy2-left">
+            <p>Case Studies</p>
+            <h3>Case Studies</h3>
+            </div>
+            <div className="casestudy2-right">
+                <h5>Which is the same as saying through shrinking from toil and pain these perfectly simple and easy to distinguish.</h5>
+            </div>
+        </div>
+        <div className="casestudy2-bottom">
+        <div className="manage-bottom-right">
+                <div className="box">
+                    <div className="box-left">
+                        <HiOutlineChartBar className='bar-lines'/>
+                    </div>
+                    <div className="box-right">
+                        <h3>Strategic Consulting Services</h3>
+                        <p>At lectus ante ultricies pretium integer amet lobortis. Arcu aliquam senectus </p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="box-left">
+                        <HiOutlineChartBar className='bar-lines'/>
+                    </div>
+                    <div className="box-right">
+                        <h3>Strategic Consulting Services</h3>
+                        <p>At lectus ante ultricies pretium integer amet lobortis. Arcu aliquam senectus </p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="box-left">
+                        <HiOutlineChartBar className='bar-lines'/>
+                    </div>
+                    <div className="box-right">
+                        <h3>Strategic Consulting Services</h3>
+                        <p>At lectus ante ultricies pretium integer amet lobortis. Arcu aliquam senectus </p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="box-left">
+                        <HiOutlineChartBar className='bar-lines'/>
+                    </div>
+                    <div className="box-right">
+                        <h3>Strategic Consulting Services</h3>
+                        <p>At lectus ante ultricies pretium integer amet lobortis. Arcu aliquam senectus </p>
+                    </div>
                 </div>
             </div>
-            <div className="case-box">
-                <img src="/images/casestudy2.png" alt="" />
-                <div className="case-text">
-                    <h6>Finance</h6>
-                    <h3>Financial Plan</h3>
+            <div className="manage-bottom-right">
+                <div className="box">
+                    <div className="box-left">
+                        <HiOutlineChartBar className='bar-lines'/>
+                    </div>
+                    <div className="box-right">
+                        <h3>Strategic Consulting Services</h3>
+                        <p>At lectus ante ultricies pretium integer amet lobortis. Arcu aliquam senectus </p>
+                    </div>
                 </div>
-            </div>
-            <div className="case-box">
-                <img src="/images/casestudy3.png" alt="" />
-                <div className="case-text">
-                    <h6>Marketing</h6>
-                    <h3>Marketing Advice</h3>
+                <div className="box">
+                    <div className="box-left">
+                        <HiOutlineChartBar className='bar-lines'/>
+                    </div>
+                    <div className="box-right">
+                        <h3>Strategic Consulting Services</h3>
+                        <p>At lectus ante ultricies pretium integer amet lobortis. Arcu aliquam senectus </p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="box-left">
+                        <HiOutlineChartBar className='bar-lines'/>
+                    </div>
+                    <div className="box-right">
+                        <h3>Strategic Consulting Services</h3>
+                        <p>At lectus ante ultricies pretium integer amet lobortis. Arcu aliquam senectus </p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="box-left">
+                        <HiOutlineChartBar className='bar-lines'/>
+                    </div>
+                    <div className="box-right">
+                        <h3>Strategic Consulting Services</h3>
+                        <p>At lectus ante ultricies pretium integer amet lobortis. Arcu aliquam senectus </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from '../components/About/About'
+import CaseStudy2 from '../components/casestudy/CaseStudy2'
 import Footer from '../components/Footer/Footer'
 import Homebanner2 from '../components/Homebanner/Homebanner2'
 import Numbers from '../components/Numbers/Numbers'
@@ -12,6 +13,7 @@ const AboutUs = () => {
         <Topbar/>
         <Homebanner2  title='Home/About-us' text='About us'/>
         <About/>
+        <CaseStudy2/>
         <Numbers/>
         <Team2/>
         <Footer/>
